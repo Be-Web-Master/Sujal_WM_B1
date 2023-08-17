3bccd01c2c09bdf1ff34810ccfd199503fca373d
Obliger0 <sujalgotharwal904@gmail.com>
2023-08-17 20:21:09 +0530

    task2

---
+++ scritpt.js
@@ -0,0 +1,21 @@
+const studentsMarks = [
+    [80, 70, 55],
+    [45, 90, 75],
+    [70, 0, 60]
+];
+
+function calculateQuizResults(studentsMarks) {
+    let pass = 200, resultArr = [];
+    for (let i = 0; i < studentsMarks.length; i++) {
+        let totalMark = 0;
+        for (let j = 0; j < studentsMarks[i].length; j++) {
+            totalMark += studentsMarks[i][j];
+        }
+        
+        if (totalMark >= pass) resultArr[i] = [totalMark,"pass"];
+        else resultArr[i] = [totalMark, "fail"];
+    }
+    console.log(resultArr);
+}
+
+calculateQuizResults(studentsMarks)
