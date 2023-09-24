520cd8bd3a50b32a6848a92a2663a4461b83dae3
Obliger0 <sujalgotharwal904@gmail.com>
2023-09-25 00:36:15 +0530

    table application

--- react-project-purchaseorder/src/App.js
+++ react-project-purchaseorder/src/App.js
@@ -1,19 +1,31 @@
-import { useState } from 'react';
+import { useState, createRef, useEffect } from 'react';
 import './App.css';
-import ExpiryDateComponent from './expiryDateComponent';
-import TableComponent from './tableComponent';
+import TableComp from './table';
+// import ExpiryDateComponent from './expiryDateComponent';
+// import TableComponent from './tableComponent';
+// import { SlabComponent } from './slab';
+
 
 function App() {
-  const [table, setTable ] = useState([]);
-  console.log({table});
+  const [modal, setModal] = useState({ open: false, type: '' });
+  const divApp = createRef();
+  useEffect(() => {
+    modal.open ? divApp.current.style.opacity = "0.5" : divApp.current.style.opacity = "1"
+  })
   return (
-    <div className="App">
+    <div ref={divApp} className='App'>
       <header className="App-header">
-        <ExpiryDateComponent setTable={setTable} table={table}/>
-        <TableComponent table={table}/>
+        <TableComp setModal={setModal} modal={modal} />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+// const [table, setTable ] = useState([]);
+// {/* <ExpiryDateComponent setTable={setTable} table={table}/>
+// <TableComponent table={table}/> */}
+// {/* <SlabComponent/> */}
+
