91cb6fd2d84bd712169c558ea5fd40c0cce5e12b
Obliger0 <sujalgotharwal904@gmail.com>
2023-09-27 11:38:44 +0530

    ques 5 with swap

--- ques5.js
+++ ques5.js
@@ -7,19 +7,18 @@ const arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
 
 
 function moveNegativeNums(arr) {
-    const arr1=[], arr2=[];
-    for (let i = 0; i < arr.length; i++) {
-        (arr[i]<0) ? arr1.push(arr[i]): arr2.push(arr[i])
-        // console.log(arr);
-        // if(arr[i-1]>0){
-        //     const temp = arr[i-1];
-        //     arr[i-1] = arr[i]
-        //     arr[i] = temp;
-        // }
+    // const arr1=[], arr2=[];
+    for (let i = 1; i < arr.length; i++) {
+        if(arr[i-1]>0 && arr[i]<0){
+            const temp = arr[i - 1];
+            arr[i - 1] = arr[i]
+            arr[i] = temp;
+        }
     }
-
-    return arr1.concat(arr2);
+  
+    // return arr;
 }
-
-// console.log(arr);
+    
+    
 console.log(moveNegativeNums(arr));
+// (arr[i]<0) ? arr1.push(arr[i]): arr2.push(arr[i])
