0425e9f80c2186c821f80739b2f2ef49330a4468
Obliger0 <sujalgotharwal904@gmail.com>
2023-09-28 11:39:42 +0530

    ques 1

---
+++ ques1.js
@@ -0,0 +1,22 @@
+//Find the first missing number in the sorted array
+// function findFirstMissingNumber(arr) - this function will return the first first number, if we don't have any missing number then return the last expected number
+// const arr = [4,5,8,9,10]
+// output - 6
+
+// const arr = [11,12,13,15]
+// output - 14
+
+
+// const arr = [4,5,8,9,10]
+const arr = [11,12,13,14,15,16]
+
+function findFirstMissingNumber(arr){
+    let start = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]!==start) return start;
+        ++start;;
+    }
+    return arr[arr.length-1]+1;
+}
+
+console.log(findFirstMissingNumber(arr));
