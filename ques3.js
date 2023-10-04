589a6675a59c579e3c841bab653bde6e1cd0bb6d
Obliger0 <sujalgotharwal904@gmail.com>
2023-10-04 11:50:14 +0530

    ques3

---
+++ ques3.js
@@ -0,0 +1,22 @@
+// Implement Bubble Sort
+// const arr = [11,4,6,14,2,51,1]
+// output - [1,2,4,6,11,14,51]
+// function bubbleSort(arr) - this function will return sorted array
+
+
+const arr = [11,4,6,14,2,51,1]
+
+function bubbleSort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            if(i!==j && arr[i]>arr[j]){
+                const temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr));
