57f2a18c7a5601f3583932e2d74a6adf7f9e5856
Obliger0 <sujalgotharwal904@gmail.com>
2023-10-25 11:40:19 +0530

    ques1 & ques2

---
+++ ques1.js
@@ -0,0 +1,27 @@
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
+// Example 1:
+
+// Input: word = "USA"
+// Output: true
+// Example 2:
+
+// Input: word = "FlaG"
+// Output: false
+
+function isCapitalRight(word) {
+    const temp1 = word;
+    if (temp1.toUpperCase() === word) return true;
+    const temp2 = word;
+    if (temp2.toLowerCase() === word) return true;
+    const str = word.charAt(0).toUpperCase() + word.toLowerCase().substring(1);
+    if(str===word) return true;
+    return false;
+}
+
+console.log(isCapitalRight("Google"));
